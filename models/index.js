2064c7b012fa710ee3452a61f4f3cb905ecef746
RussellBrookman <hr.brookman@gmail.com>
2018-01-24 06:21:03 -0800

    done

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
-// this object should contain all of my models
-
 module.exports = {
-  Article: require("./Article"),
-  Note: require("./Note")
+  Article: require('./Article'),
+  Note: require('./Note')
 };
--- server.js
+++ server.js
@@ -1,46 +1,23 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var cheerio = require("cheerio");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4127;
+const app = express();
 
-var app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
-var db = require("./models");
-
-// middlewear config
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-
-// express.static to make public folder a static directory
-app.use(express.static("public"));
-
-// body-parser for handling form submissions
-// extended : false			This means I can't post nested objects.
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// setting up handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controlers/nbcNews.js");
-
-app.use("/", routes);
-
-app.listen(port);
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost@ds040877.mlab.com:40877/national_news_test", {
-  useMongoClient: true
-});
+// mongoose.connect(MONGODB_URI, { useMongoClient: true });
+mongoose.connect(MONGODB_URI);
 
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+require('./controllers/controller.js')(app);
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`) );
